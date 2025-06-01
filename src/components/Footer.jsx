01c76e147abc29fab2e98da9b96efe9ceae0c01f
SamueleMaka <samuele.maka.imt@gmail.com>
2025-06-01 23:46:43 +0200

    feat: footer finito

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -5,15 +5,15 @@ function AppFooter() {
     <>
         <div className={style.footerContainer}>
             <div className={style.container}>
-                <div className={buttonContainer}>
+                <div className={style.buttonContainer}>
                     <button>SIGN-UP NOW!</button>
                 </div>
-                <div className={socialContainer}>
+                <div className={style.socialContainer}>
                     <a href="">FOLLOW US</a>
                     <a href=""><img src="footer-facebook.png" alt="" /></a>
                     <a href=""><img src="footer-twitter.png" alt="" /></a>
                     <a href=""><img src="footer-youtube.png" alt="" /></a>
-                    <a href=""><img src="footer-pintrest.png" alt="" /></a>
+                    <a href=""><img src="footer-pinterest.png" alt="" /></a>
                     <a href=""><img src="footer-periscope.png" alt="" /></a>
                 </div>
             </div>
