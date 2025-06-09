313d0a605f64ebf8736d46d52eadb22fdb9836f2
SamueleMaka <samuele.maka.imt@gmail.com>
2025-06-09 22:51:24 +0200

    feat: header dinamico

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,6 +1,19 @@
 import style from "./Header.module.css"
 
 function AppHeader() {
+    const linkLabels = [
+        "Character",
+        "Comics",
+        "Movies",
+        "TV",
+        "Games",
+        "Collectibles",
+        "Videos",
+        "Fans",
+        "News",
+        "Shop"
+      ];
+      
     return(
     <>
     <div className={style.container}>
@@ -9,18 +22,9 @@ function AppHeader() {
                 <img src="dc-logo.png" alt="" />
             </div>
             <div className={style.linkContainer}>
-                <ul>
-                    <li><a href="">Character</a></li>
-                    <li><a href="">Comics</a></li>
-                    <li><a href="">Movies</a></li>
-                    <li><a href="">TV</a></li>
-                    <li><a href="">Games</a></li>
-                    <li><a href="">Collectibles</a></li>
-                    <li><a href="">Videos</a></li>
-                    <li><a href="">Fans</a></li>
-                    <li><a href="">News</a></li>
-                    <li><a href="">Shop</a></li>
-                </ul>
+                {linkLabels.map((currLink, index) => (
+                    <li key={"linkHeade" + index}><a href="">{currLink}</a></li>
+                ))}
             </div>
         </div>
     </div>  
